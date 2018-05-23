b21ed6b4f59725a44218aabb8c521ba6839a2702
Marcian Diamond <marciandmnd@yahoo.com>
2018-05-23 11:14:40 -0700

    Add custom polyfill for browsers that dont support navigatro.getUserMedia()

--- public/src/js/feed.js
+++ public/src/js/feed.js
@@ -5,10 +5,37 @@ const sharedMomentsArea = document.querySelector('#shared-moments');
 const form = document.querySelector('form');
 const titleInput = document.querySelector('#title');
 const locationInput = document.querySelector('#location');
+const videoPlayer = document.querySelector('#player');
+const canvasElement = document.querySelector('#canvas');
+const captureButton = document.querySelector('#capture-btn');
+const imagePicker = document.querySelector('#image-picker');
+const imagePickerArea = document.querySelector('#pick-image');
+
+function initializeMedia() {
+  if(!('mediaDevices' in navigator)) {
+    navigator.mediaDevices = {};
+  }
+
+  if(!('getUserMedia' in navigator.mediaDevices)) {
+    navigator.mediaDevices.getUserMedia = function(constraints) {
+      const getUserMedia = navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+      
+      if(!getUserMedia) {
+        return Promise.reject(new Error('getUserMedia is not implemented'));
+      }
+
+      return new Promise(function(resolve, reject) {
+        getUserMedia.call(navigator, constraints, resolve, reject)
+      });
+    }
+  }
+
+}
+
 
 function openCreatePostModal() {
-    createPostArea.style.transform = 'translateY(0)';
-
+  createPostArea.style.transform = 'translateY(0)';
+  initializeMedia();
   if (deferredPrompt) {
     deferredPrompt.prompt();
 
